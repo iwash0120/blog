522cefb85020c872c313c37d2489683d2cd6815e
yuto.iwashita <y120sb@gmail.com>
2022-05-08 21:19:03 +0900

    initial commit

---
+++ api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello AMD64</h1>")
+}
+
+func main() {
+	portNum := os.Getenv("PORT")
+	if portNum == "" {
+		portNum = "8080"
+	}
+	port := fmt.Sprintf(":%s", portNum)
+	fmt.Println(port)
+	http.HandleFunc("/", helloHandler)
+	http.ListenAndServe(port, nil)
+}
